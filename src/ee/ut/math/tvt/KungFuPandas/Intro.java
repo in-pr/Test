f37f74c7e12ada7681874ab4a87ce8fe5631518a
in-pr <in-pr@hotmail.com>
2013-10-14 22:19:48 +0300

    testt

---
+++ src/ee/ut/math/tvt/KungFuPandas/Intro.java
@@ -0,0 +1,10 @@
+package ee.ut.math.tvt.KungFuPandas;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ src/ee/ut/math/tvt/KungFuPandas/IntroUI.java
@@ -0,0 +1,10 @@
+package ee.ut.math.tvt.KungFuPandas;
+
+public class IntroUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
